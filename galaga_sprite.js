7ea470b04b2e653c913f0330d5168038437e3c6b
nbm <nbmoss@uga.edu>
2014-01-29 01:41:57 -0500

    need to add enemy bullets and formations/generic functions/scoring

--- galaga_sprite.js
+++ galaga_sprite.js
@@ -1,16 +1,16 @@
 
 function GalagaSprite(args){
 	this.image = new Image();
-	this.image.src = args.src;
+	this.image.src = args.imgSrc;
 	this.x = args.x;
 	this.y = args.y;
 	this.imgFrames = args.frames;
 	this.frame = 0;
 
-	this.draw = function(){
+	this.draw = function(ctx){
 		ctx.drawImage(this.image, this.frame, 0, args.spw,
                   args.spw, this.x, this.y, args.spw, args.spw);
-    	this.frame += 48;
+    	this.frame += 24;
     	if( this.frame >= this.image.width ){
     		this.frame = 0;
     	}
